242ad078e9ff5f43ec6e8663ce50caf64c019404
electricline <jktmobile6637@gmail.com>
2021-01-06 00:46:42 +0900

    ﻿test: 회원 도메인 테스트
    <description>
    <issue-number>

---
+++ src/main/java/harden/core/MemberApp.java
@@ -0,0 +1,21 @@
+package harden.core;
+
+import harden.core.member.Grade;
+import harden.core.member.Member;
+import harden.core.member.MemberService;
+import harden.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
