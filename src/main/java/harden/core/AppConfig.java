04257d8424842eeb49b26ce820da8f0b8711523e
electricline <jktmobile6637@gmail.com>
2021-01-07 13:46:30 +0900

    ﻿feature : spring configuration 적용
    <description>
    <issue-number>

--- src/main/java/harden/core/AppConfig.java
+++ src/main/java/harden/core/AppConfig.java
@@ -2,28 +2,36 @@ package harden.core;
 
 import harden.core.discount.DiscountPolicy;
 import harden.core.discount.FixDiscountPolicy;
+import harden.core.discount.RateDiscountPolicy;
 import harden.core.member.MemberRepository;
 import harden.core.member.MemberService;
 import harden.core.member.MemberServiceImpl;
 import harden.core.member.MemoryMemberRepository;
 import harden.core.order.OrderService;
 import harden.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/harden/core/MemberApp.java
+++ src/main/java/harden/core/MemberApp.java
@@ -4,12 +4,18 @@ import harden.core.member.Grade;
 import harden.core.member.Member;
 import harden.core.member.MemberService;
 import harden.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+ 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
