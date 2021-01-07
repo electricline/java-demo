e59cd22a139ec46b5db3897d04eed595f1de717e
electricline <jktmobile6637@gmail.com>
2021-01-07 09:58:45 +0900

    ﻿feature, test: 정률할인정책 기능 및 테스트 개발
    <description>
    <issue-number>

---
+++ src/main/java/harden/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,20 @@
+package harden.core.discount;
+
+import harden.core.member.Grade;
+import harden.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+
+    }
+}
