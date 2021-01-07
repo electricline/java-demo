7cef26a72fc44106eb46ca1b1f3a6296a35ba5ff
electricline <jktmobile6637@gmail.com>
2021-01-07 11:31:28 +0900

    ﻿update : appconfig 리팩토링
    <description>
    <issue-number>

--- src/main/java/harden/core/AppConfig.java
+++ src/main/java/harden/core/AppConfig.java
@@ -1,6 +1,8 @@
 package harden.core;
 
+import harden.core.discount.DiscountPolicy;
 import harden.core.discount.FixDiscountPolicy;
+import harden.core.member.MemberRepository;
 import harden.core.member.MemberService;
 import harden.core.member.MemberServiceImpl;
 import harden.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import harden.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
