9bfe6ae237d3645c2426d64264f3174dcf0d52d3
electricline <jktmobile6637@gmail.com>
2021-01-10 20:11:01 +0900

    ﻿update :lomobk refator
    <description>
    <issue-number>

--- src/main/java/harden/core/order/OrderServiceImpl.java
+++ src/main/java/harden/core/order/OrderServiceImpl.java
@@ -1,36 +1,18 @@
 package harden.core.order;
 
 import harden.core.discount.DiscountPolicy;
-import harden.core.discount.FixDiscountPolicy;
-import harden.core.discount.RateDiscountPolicy;
 import harden.core.member.Member;
 import harden.core.member.MemberRepository;
-import harden.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존 하도록 설계 변경
 
-
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy){
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
