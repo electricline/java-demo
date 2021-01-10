0a4faf48aa310b0e28cb0e77473f348a7af0ce3f
electricline <jktmobile6637@gmail.com>
2021-01-10 22:09:01 +0900

    ﻿test : bean 2개 이상일 때logic
    <description>
    <issue-number>

--- src/main/java/harden/core/order/OrderServiceImpl.java
+++ src/main/java/harden/core/order/OrderServiceImpl.java
@@ -4,18 +4,19 @@ import harden.core.annotation.MainDiscountPolicy;
 import harden.core.discount.DiscountPolicy;
 import harden.core.member.Member;
 import harden.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-    
-    @MainDiscountPolicy
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존 하도록 설계 변경
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override
--- src/test/java/harden/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/harden/core/discount/RateDiscountPolicyTest.java
@@ -12,14 +12,14 @@ class RateDiscountPolicyTest {
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
-    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    @DisplayName("VIP는 20% 할인이 적용되어야 한다")
     void vip_o() {
         // given
         Member member = new Member(1L, "memberVIP", Grade.VIP);
         // when
         int discount = discountPolicy.discount(member, 10000);
         // then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(2000);
     }
 
     @Test
