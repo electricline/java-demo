87a66be6ff2f4e510eb680bde7f418e5d87f61f9
electricline <jktmobile6637@gmail.com>
2021-01-21 20:29:27 +0900

    feature - scope의 proxyMode 적용

--- src/main/java/harden/core/web/LogDemoController.java
+++ src/main/java/harden/core/web/LogDemoController.java
@@ -3,7 +3,6 @@ package harden.core.web;
 
 import harden.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> objectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = objectProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
